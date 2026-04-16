0986e6203983ba5e77e6dc48747f029a1b836e83
shubhamkjadhav19-ops <shubhamkjadhav19@gmail.com>
2026-04-16 18:29:58 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/blogapp/config/SecurityConfig.java
+++ src/main/java/com/example/blogapp/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "https://blog-production-3d74.up.railway.app"
+                "https://blog-production-3d74.up.railway.app","https://sjvblog.netlify.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
